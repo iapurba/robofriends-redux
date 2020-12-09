611da8508da8e6fb5033413f7b404d7da8bea99a
Apurba Panja <apurbapanja1@gmail.com>
2020-12-09 18:08:00 +0530

    Updated for redux implementation

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc grow bg-light-blue br3 pa4 ma2 dib bw2 shadow-5'>
-      <img className='grow ba b--blue bw1 br-100 pa2'
+    <div className='tc grow bg-light-green br3 pa4 ma2 dib bw2 shadow-5'>
+      <img className='grow ba b--green bw1 br-100 pa2'
       alt='robots' src={`https://robohash.org/${id}?size=150x150`} />
       <div>
         <h2>{name}</h2>
