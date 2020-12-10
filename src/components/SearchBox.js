40c6f83e5f59d7bd7b85843ab83a28a1f14741c3
Apurba Panja <apurbapanja1@gmail.com>
2020-12-10 18:29:59 +0530

    Updated for accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,15 +3,13 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-    <label for='search-robots'>
       <input
-        id='search-robots'
+        ariaLabel='search-robots'
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
       />
-    </label>
     </div>
   );
 }
