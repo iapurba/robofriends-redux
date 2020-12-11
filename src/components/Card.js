b109fd37289a3bb0140582ba83c455531c009453
Apurba Panja <apurbapanja1@gmail.com>
2020-12-11 17:50:08 +0530

    Updated for PWA

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc grow bg-light-yellow br3 pa4 ma2 dib bw2 shadow-5'>
+    <div className='grow bg-light-yellow br3 pa4 ma2 dib bw2 shadow-5'>
       <img className='grow ba b--green bw1 br-100 pa2 mh4'
       alt='robots' src={`https://robohash.org/set=set5/${id}?size=200x200`} />
       <div>
